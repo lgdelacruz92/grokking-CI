0c4585791c0f5e1483ea38180b19766b0d1c5667
Lester <lgdelacruz92@gmail.com>
2024-05-24 08:44:37 -0400

    no memo

--- dynamic-programming/combination-sum.py
+++ dynamic-programming/combination-sum.py
@@ -1,19 +1,22 @@
-def dfs(nums, i, s, t, cur, res):
+def dfs(nums, i, s, t, cur, res, memo):
+    if nums[i] in memo:
+        return
     if s > t:
         return
     if i >= len(nums) or s == t:
         if s == t:
             res.append(cur[:])
         return
-    
+    memo.add(nums[i])
     cur.append(nums[i])
-    dfs(nums, i, s + nums[i], t, cur, res)
+    dfs(nums, i, s + nums[i], t, cur, res, memo)
     cur.pop()
-    dfs(nums, i+1, s, t, cur, res)
+    dfs(nums, i+1, s, t, cur, res, memo)
 
 def combination_sum(nums, target):
     res = []
-    dfs(nums, 0, 0, target, [], res)
+    memo = set()
+    dfs(nums, 0, 0, target, [], res, memo)
     return res
 
 if __name__ == '__main__':
