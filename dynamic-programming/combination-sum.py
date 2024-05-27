7d8a366ae5b28266e30b8003ec9b9a9e9bf93c62
Lester <lgdelacruz92@gmail.com>
2024-05-27 08:52:26 -0400

    word break

--- dynamic-programming/combination-sum.py
+++ dynamic-programming/combination-sum.py
@@ -1,26 +1,27 @@
-def dfs(nums, i, s, t, cur, res, memo):
-    if nums[i] in memo:
-        return
-    if s > t:
-        return
-    if i >= len(nums) or s == t:
-        if s == t:
-            res.append(cur[:])
-        return
-    memo.add(nums[i])
-    cur.append(nums[i])
-    dfs(nums, i, s + nums[i], t, cur, res, memo)
-    cur.pop()
-    dfs(nums, i+1, s, t, cur, res, memo)
-
 def combination_sum(nums, target):
-    res = []
-    memo = set()
-    dfs(nums, 0, 0, target, [], res, memo)
-    return res
+    dp = [[] for _ in range(target+1)]
+    dp[0].append([])
+
+    for t in range(target+1):
+        sub_t_comb_set = set()
+        for n in nums:
+            if t-n >= 0:
+                sub_row = dp[t-n]
+                if sub_row:
+                    for r in sub_row:
+                        r_cp = r[:]
+                        r_cp.append(n)
+                        r_cp.sort()
+                        r_cp_set = tuple(r_cp)
+                        sub_t_comb_set.add(r_cp_set)
+                else:
+                    if n == t:
+                        sub_t_comb_set.add(tuple([n]))
+        dp[t] = [list(comb) for comb in sub_t_comb_set]
+    return dp[target]
 
 if __name__ == '__main__':
     a = [2,3,4]
     t = 6
-    res = combination_sum(a, t)
+    res = combination_sum(a,t)
     print(res)
